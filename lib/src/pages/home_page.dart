a0d4fd930888593d3681ab70b535bc71a304f559
Luis Columna <luis.columna10@gmail.com>
2019-08-22 18:02:41 -0500

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HommePage extends StatelessWidget {
+
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Preferencias de Usuario')
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario'),
+          Divider(),
+          Text('Género'),
+          Divider(),
+          Text('Nombre Usuario'),
+          Divider(),
+        ]
+      )
+    );
+  }
+
+
+}
