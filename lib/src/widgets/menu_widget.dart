75ed863288f25dc61b0e67eb2e0bf76b7dcfa855
Luis Columna <luis.columna10@gmail.com>
2019-08-28 11:17:51 -0500

    app terminada

--- lib/src/widgets/menu_widget.dart
+++ lib/src/widgets/menu_widget.dart
@@ -1,39 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:preferenciasusuarioapp/src/share_prefs/preferencias_usuario.dart';
-import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
+import 'package:preferenciasusuarioapp/src/pages/home_page.dart';
+import 'package:preferenciasusuarioapp/src/pages/settings_page.dart';
 
-class HommePage extends StatelessWidget {
-
-  static final String routeName = 'home';
-  final prefs = new PreferenciasUsuario();
+class MenuWidget extends StatelessWidget {
+  const MenuWidget({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  
-  prefs.ultimaPagina = HommePage.routeName;
-
-    return Scaffold(
-      appBar: AppBar(
-        title:Text('Preferencias de Usuario'),
-        backgroundColor: (prefs.colorSecundario) ? Colors.teal : Colors.blue,
-      ),
-      drawer: MenuWidget(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Color secundario: ${ prefs.colorSecundario }'),
-          Divider(),
-          Text('Género: ${prefs.genero}'),
-          Divider(),
-          Text('Nombre Usuario: ${ prefs.nombreUsuario }'),
-          Divider(),
-        ]
-      )
-    );
-  }
-
-  /* Drawer _crearMenu(BuildContext context) {
-
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -73,7 +46,5 @@ class HommePage extends StatelessWidget {
         ],
       ),
     );
-  } */
-
-
+  }
 }
